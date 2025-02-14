27436cfcce2a8f56e10df0c42fb126879658092f
kamal <rmi.search@gmail.com>
2025-02-14 17:26:52 +0100

    Configuration firebase basic

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,13 +1,15 @@
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
+
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCf8LKxGz4j1cnwFwuPGb2y8xtaakDw0T0",
+  authDomain: "nksmvptms.firebaseapp.com",
+  projectId: "nksmvptms",
+  storageBucket: "nksmvptms.firebasestorage.app",
+  messagingSenderId: "927946384672",
+  appId: "1:927946384672:web:00f5d0784dbceaa68d2345",
+  measurementId: "G-0SWQS09982"
 };
 
 // Initialize Firebase only if it hasn't been initialized yet
